29dff5435273b72053e6532f31823d058110cacd
AntoineHaller <34535243+AntoineHaller@users.noreply.github.com>
2020-02-16 18:34:43 +0100

    Update golf.route.js

--- src/routes/golf.route.js
+++ src/routes/golf.route.js
@@ -1,19 +1,20 @@
 const app = require('express');
 const golf = require('../controllers/golf.controller');
 const routes = app.Router();
+const jwt_token = require('../jwt/token.jwt.js');
 
 //Création
-routes.post('/golf', golf.create);
+routes.post('/golf', jwt_token.authToken, golf.create);
 
 //Affichage
 routes.get('/golf', golf.findAll);
 routes.get('/golf/:id', golf.findOne);
 
 //Modifier
-routes.post('/golf/:id', golf.update);
+routes.post('/golf/:id', jwt_token.authToken, golf.update);
 
 //Suppression
-routes.delete('/golf/:id', golf.deleteOne);
-routes.delete('/golf', golf.deleteMany)
+routes.delete('/golf/:id', jwt_token.authToken, golf.deleteOne);
+routes.delete('/golf', jwt_token.authToken, golf.deleteMany)
 
 module.exports = routes;
