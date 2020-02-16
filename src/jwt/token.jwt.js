2157d5e706756d4ac49bfa7756f93de620392922
AntoineHaller <34535243+AntoineHaller@users.noreply.github.com>
2020-02-16 18:33:28 +0100

    Update token.jwt.js

--- src/jwt/token.jwt.js
+++ src/jwt/token.jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 //authentification token
-exports.authToken(req, res, next) {
+exports.authToken = (req, res, next) => {
   let token = req.headers['x-access-token'];
   if (!token) {
     return res.status(400).send({
